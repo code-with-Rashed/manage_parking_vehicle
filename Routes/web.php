f003fafec100fbe3d83f40f389eaaa8a2b0322a5
Rashed alam <rashedalamj@gmail.com>
2023-04-02 15:42:36 +0600

    vehicle category releted Routers added

--- Routes/web.php
+++ Routes/web.php
@@ -2,6 +2,12 @@
 
 use Management\Classes\Router;
 
-Router::get("/mvc/view",function(){
- return view("testView");
-});
+// Vehicle Category Routers 
+use App\Controllers\VehicleCategoryController;
+Router::get("/category",[VehicleCategoryController::class,"index"]);
+Router::get("/add/category",[VehicleCategoryController::class,"add_vehicle_category"]);
+Router::get("/edit/category/{id}",[VehicleCategoryController::class,"edit_vehicle_category"]);
+Router::get("/delete/category/{id}",[VehicleCategoryController::class,"delete"]);
+Router::post("/add/category",[VehicleCategoryController::class,"insert"]);
+Router::post("/update/category/{id}",[VehicleCategoryController::class,"update"]);
+//--------------------------
