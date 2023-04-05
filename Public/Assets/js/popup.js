1d964b33f4512a506ce8ac00bd37f8fc6fcc714a
Rashed alam <rashedalamj@gmail.com>
2023-04-05 11:13:34 +0600

    new function added

--- Public/Assets/js/popup.js
+++ Public/Assets/js/popup.js
@@ -3,10 +3,14 @@
 const popup = document.querySelector('.popup');
 const confirm_btn = document.querySelector('.confirm-btn');
 
-function confirm_delete(delete_link) {
-    confirm_btn.href = delete_link;
+function popup_open() {
     popup.style.display = 'block';
 }
 function popup_close() {
     popup.style.display = 'none';
+}
+
+function confirm_delete(delete_link) {
+    confirm_btn.href = delete_link;
+    popup_open();
 }
